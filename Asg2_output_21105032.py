97f550bb57962f97ebf45fe1c3d1cc8e1a859893
AVI GARG_21105032 <97087858+AvixGarg@users.noreply.github.com>
2022-01-21 22:15:08 +0530

    Add files via upload

---
+++ Asg2_output_21105032.py
@@ -0,0 +1,43 @@
+Answer 1 
+
+(a)	The length of the given string is : 35
+(b)	The string in reverse would be : egaugnal evitisnes esac a si nohtyP
+(c)	The new string becomes : a case sensitive
+(d)	The replaced substring will be : Python is object oriented language
+(e)	The first occurence of the given substring "a" is at index no. = 10
+(f)	The inputted strings with no white spaces will be "Pythonisacasesensitivelanguage"
+
+
+ Answer 2 
+
+Hey, AVI GARG Here!
+My SID is 21105032
+I am from Electronics and Communication Engineering department and my CGPA is 9.4
+
+ Answer 3 
+
+a&b= 8
+a|b= 58
+a^b= 50
+a<<2= 224 and b<<2= 40
+a>>2= 14 and b>>4= 0
+ 
+ Answer 4 
+
+Enter the  first number : 987
+Enter the second number : 4994
+Enter the third number : 4876
+The greatest number is 4994
+
+ 
+ Answer 5 
+
+Enter the string : udxyfvsnameirbagigaie
+Yes
+
+
+ Answer 6 
+
+Enter the dimensions to check whether triangle possible or not : 3  6 6
+Yes
+

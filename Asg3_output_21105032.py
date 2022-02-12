a0631dde9772af13af27468066a01efcabe3bce1
AVI GARG_21105032 <97087858+AvixGarg@users.noreply.github.com>
2022-02-12 14:13:42 +0530

    Add files via upload

---
+++ Asg3_output_21105032.py
@@ -0,0 +1,106 @@
+
+Answer 1
+Enter the string:we don't know why,we don't know how but baby tonight we're beautiful now
+
+Dictionary containing {'Word':'Word Count'} is shown below:
+{'why,we': 1, 'baby': 1, 'tonight': 1, 'but': 1, "don't": 2, 'know': 2, 'how': 1, 'we': 1, 'now': 1, "we're": 1, 'beautiful': 1}
+
+Answer 2
+Enter Date below to get date of next day.
+
+Enter Day [1-31]:28
+Enter Month [1-12]:6
+Enter Year [1800-2025]:2003
+
+Next Date in format Day/Month/Year is: 29/6/2003
+
+Answer 3
+Enter the numbers separated by space:1 3 5 7 9
+
+List containing (n,n^2) is shown below:
+[(1, 1), (3, 9), (5, 25), (7, 49), (9, 81)]
+
+Answer 4
+Enter grade points:9
+Your grade is 'A' and Excellent performance.
+
+Answer 5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Answer 6
+Enter student name:Avi Garg
+Enter student SID:21105032
+Enter Y to continue or N to end:y
+Enter student name:Pankaj
+Enter student SID:21105005
+Enter Y to continue or N to end:y
+Enter student name:Sunandun Aggarwal
+Enter student SID:21105055
+Enter Y to continue or N to end:y
+Enter student name:Naman Goyal
+Enter student SID:21105020
+Enter Y to continue or N to end:n
+
+Q.6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105032: 'Avi Garg', 21105005: 'Pankaj', 21105055: 'Sunandun Aggarwal', 21105020: 'Naman Goyal'}
+
+Q.6(b)
+The Dictionary after sorting according to name:
+{21105032: 'Avi Garg', 21105020: 'Naman Goyal', 21105005: 'Pankaj', 21105055: 'Sunandun Aggarwal'}
+
+Q.6(c)
+The Dictionary after sorting according to SID:
+{21105005: 'Pankaj', 21105020: 'Naman Goyal', 21105032: 'Avi Garg', 21105055: 'Sunandun Aggarwal'}
+
+Q.6(d)
+Enter SID to get name of student:21105032
+Name of student with SID 21105032 is Avi Garg.
+
+Answer 7
+Enter number of elements N in fibonacci series:
+[N must be positive Integer]: N=12
+
+The fibonacci series with 12 elements is shown below:
+[1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+
+Average of given fibonacci series is 31.33
+
+Answer 8
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+Q.8(a)
+
+A new Set of all 'elements that are in Set1 and Set2 but not both' is:
+{1, 3, 5, 6, 8}
+
+Q.8(b)
+
+A new set of all elements that are 'in only one of the three sets Set1,Set2 and Set3' is:
+{3, 6, 8, 9, 13, 17}
+
+Q.8(c)
+
+A new set of elements that are 'exactly in two of the sets Set1, Set2 and Set3' is:
+{1, 2, 4, 5}
+
+Q.8(d)
+
+A new set of all Integers in the 'range 1 to 10' that are 'not in Set1' is:
+{6, 7, 8, 9, 10}
+
+Q.8(e)
+
+A new set of all Integers in the range 1 to 10 that are not in Set1,Set2 and Set3.
+{10, 7}
+
+Process finished with exit code 0
